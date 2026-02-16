b9a16f3837c6eeb54ab77a52fac5adf64ef27616
Jason Stajich <jasonstajich.phd@gmail.com>
2026-02-16 10:35:22 -0800

    add claude's updates for version naming

--- src/adhesion_predict/__init__.py
+++ src/adhesion_predict/__init__.py
@@ -1,11 +1,18 @@
 """adhesionPredict - A bioinformatics package for predicting adhesion proteins."""
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Python < 3.8
+    from importlib_metadata import version
+
+__version__ = version("adhesion_predict")
 
 from adhesion_predict.io import find_fasta_files, process_fasta_file
 from adhesion_predict.model import load_model, predict, save_model, train_classifier
 
 __all__ = [
+    "__version__",
     "find_fasta_files",
     "process_fasta_file",
     "load_model",
